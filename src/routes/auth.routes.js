6ca6c247c915325e6dd201301a644fe6b5720e4d
rakhi <rakhisingh876.com@gmail.com>
2025-09-03 23:00:15 +0530

    Add register and login routes inside user router

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,6 +14,29 @@ router.post('/register',async(req,res)=>{
 
     res.status(201).json({
         message:"user registered successfully",
+        user
+    })
+
+
+    router.post('/login',async(req,res)=>{
+        const {username,password} = req.body
+
+        const isUserExists = await userModel.findOne({
+            username:username
+        })
+
+        if(!isUserExists){
+            return res.status(401).json({
+                message:"user account not found"
+            })
+        }
+
+        const isPasswordValidid = password == isUserExists.password
+        if(!isPasswordValidid){
+            return res.status(401).json({
+                message:"Invalid password"
+            })
+        }
     })
 })
 module.exports = router
