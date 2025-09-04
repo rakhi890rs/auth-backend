1caf5de8a040a01eb91ca65f71f570014704a986
rakhi <rakhisingh876.com@gmail.com>
2025-09-04 23:50:36 +0530

    Added register and login routes with JWT

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-// require('dotenv').config()
+// Load env variables
 require('dotenv').config();
-console.log("Loaded MONGODB_URL:", process.env.MONGODB_URL); // test
+
+// // Debugging logs
+// console.log("Loaded MONGODB_URL:", process.env.MONGODB_URL);
+// console.log("Loaded JWT_SECRET:", process.env.JWT_SECRET);
 
 const app = require("./src/app");
-const connectDB = require("./src/db/db")
+const connectDB = require("./src/db/db");
 
-connectDB()
-app.listen(3000,()=>{
-    console.log("server is running")
-})
+// Connect to MongoDB
+connectDB();
+
+// Start server
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,42 +1,52 @@
 const express = require("express")
 const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken");
 
 const router = express.Router()
 
-
-
-router.post('/register',async(req,res)=>{
-    const {username,password}= req.body;
+// Register
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
 
     const user = await userModel.create({
-        username,password
-    })
+        username, password
+    });
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET);
 
     res.status(201).json({
-        message:"user registered successfully",
-        user
-    })
+        message: "user registered successfully",
+        user,
+        token
+    });
+});
 
+// Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
 
-    router.post('/login',async(req,res)=>{
-        const {username,password} = req.body
+    const isUserExists = await userModel.findOne({
+        username: username
+    });
 
-        const isUserExists = await userModel.findOne({
-            username:username
-        })
+    if (!isUserExists) {
+        return res.status(401).json({
+            message: "user account not found"
+        });
+    }
 
-        if(!isUserExists){
-            return res.status(401).json({
-                message:"user account not found"
-            })
-        }
+    const isPasswordValidid = password == isUserExists.password;
+    if (!isPasswordValidid) {
+        return res.status(401).json({
+            message: "Invalid password"
+        });
+    }
 
-        const isPasswordValidid = password == isUserExists.password
-        if(!isPasswordValidid){
-            return res.status(401).json({
-                message:"Invalid password"
-            })
-        }
-    })
-})
-module.exports = router
+    res.status(200).json({
+        message: "user loggeIn successfully"
+    });
+});
+
+module.exports = router;
